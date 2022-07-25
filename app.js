f26e8fff6888e8fd36148d515e2c72ce341b9016
AdrianaFiszer <adrianafiszer@gmail.com>
2022-07-25 19:40:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const signatureBox = document.getElementById("AutorName")
+const autor = "Adriana Fiszer"
+signatureBox.textContent = `Autor: ${autor}`;
+
+// document.write(`<p>Autor: ${autor}</p>`)
